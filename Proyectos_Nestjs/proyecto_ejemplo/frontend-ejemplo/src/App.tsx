0ac74c857b69346f01c78fb8d5ef9860405a4631
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2025-12-29 16:35:02 -0500

    change de un modul app.tsx

--- Proyectos_Nestjs/proyecto_ejemplo/frontend-ejemplo/src/App.tsx
+++ Proyectos_Nestjs/proyecto_ejemplo/frontend-ejemplo/src/App.tsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '.pages/Home'
-import UsersPage from './pages/users/UsersPage'
-import ProductPage from './pages/products/ProductsPage'
+//import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+//import UsersPage from './pages/users/UsersPage'
+//import ProductPage from './pages/products/ProductsPage'
 //import UsersPage from './pages/users/UsersPage'
 function App() {
   return <Home/>
