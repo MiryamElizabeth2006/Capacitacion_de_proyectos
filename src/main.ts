4adae142a9b0b6f6cd92e9ae969847e85791516f
MiryamElizabeth2006 <miryamelizabeth367@gmail.com>
2025-12-29 16:37:43 -0500

    Files backend change

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:5173', //Puerto del frontend
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true
+  })
+
+  await app.listen(3000)
 }
 bootstrap();
